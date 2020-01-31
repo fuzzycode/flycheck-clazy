586a7df2ed0ffbc0230275732ef8a8cd0acc72e8
Björn Larsson <b.e.larsson@gmail.com>
2020-01-31 19:04:21 +0100

    Add basic flycheck skeleton

--- flycheck-clazy.el
+++ flycheck-clazy.el
@@ -4,5 +4,22 @@
 
 ;;; Code:
 
+(require 'flycheck)
+
+
+(flycheck-define-checker c/c++-clazy
+  "A flycheck backend for the clazy code analyzer"
+  :command ("clazy-standalone"
+            )
+  :modes (c-mode c++-mode)
+  :predicate (lambda () buffer-file-name)
+  :next-checkers ((error . c/c++-cppcheck)))
+
+;;;###autoload
+(defun flycheck-clazy-setup ()
+  "Setup flycheck clazy."
+  (add-to-list 'flycheck-checkers 'c/c++-clazy t))
+
+(provide 'flycheck-clazy)
 
 ;;; flycheck-clazy.el ends here
