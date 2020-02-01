159e81068dd2b4bf7866825645dc26c39f326baa
Björn Larsson <b.e.larsson@gmail.com>
2020-02-01 09:25:43 +0100

    Update documentation

--- flycheck-clazy.el
+++ flycheck-clazy.el
@@ -1,6 +1,35 @@
 ;;; flycheck-clazy.el --- Flycheck integration for clazy static checking -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2020 Björn Larsson
+
+;; Author: Björn Larsson <develop@bjornlarsson.net>
+;; Homepage:
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "25.1")  (dash "2.17.0") (flycheck "32"))
+;; Keywords: convenience, languages, tools
+
+;; This file is not part of GNU Emacs.
+
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
 ;;; Commentary:
+
+;;; Installation:
 
 ;;; Code:
 
@@ -18,13 +47,14 @@
   :safe (lambda (x) (-all? 'stringp x)))
 
 (flycheck-def-option-var flycheck-clazy-root nil c/c++-clazy
-  ""
+  "Root directory to use as the current working directory for the tool. If not nil it will always be used.
+If nil, `flycheck-clazy' will try to automatically determine the root directory using `flycheck-clazy--find-project-root'"
   :type 'directory
   :safe #'stringp)
 
 (defun flycheck-clazy--find-project-root (_checker)
   "Determine the project root for CHECKER using in the following order:
-`flycheck-clazy-root`, `projectile-root`, location of a `compile_commands.json`, `vc-root` and current directory."
+`flycheck-clazy-root', `projectile-root', location of a `compile_commands.json', `vc-root' and current directory."
   (let ((project-root flycheck-clazy-root))
     (when (and (not project-root)
                (member 'projectile-mode minor-mode-list))
